60d29b92a02c2a244b9e75e00e8ffdb3d23462f0
AngelDiaz <dariodiaz.3005@gmail.com>
2020-06-19 11:38:37 -0500

    BALANCEO <Funciona mal>

--- ARBOLBINARIO_PRACTICA.cpp
+++ ARBOLBINARIO_PRACTICA.cpp
@@ -19,9 +19,11 @@ struct NODE {
 class CTREE {
 public:
 	NODE* m_root;
+	int altura;
 
 	CTREE(){
 		m_root = 0;
+		altura = 0;
 	}
 
 	bool find(int x, NODE**& p) {
@@ -32,28 +34,73 @@ public:
 		return *p != 0;
 	}
 
-	bool find(int x, NODE**& p, NODE**& recorrido) {
+	//SOBRECARGA DEL METODO FIND PARA REGISTRAR EL RECORRIDO
+	bool find(int x, NODE**& p, NODE***& recorrido, int& recorridoTam) {
 		p = &m_root;
 		int i = 0;
-		recorrido[i] = *p ;
+		recorrido[i] = p ;
 
 		while (*p && (*p)->valor != x) {
 			p = &((*p)->nodes[x > (*p)->valor]);
 			i++;
-			recorrido[i] = *p;
+			recorrido[i] = p;
+			recorridoTam++;
 		}
+
 		return *p != 0;
 	}
 
 	bool insert(int x) {
 		NODE** p;
-		NODE** recorrido = new NODE* [100];
+		//HACIENDO LISTA CON EL RECORRIDO DEL NODO QUE SE VA A INSERTAR
+		NODE*** recorrido = new NODE * *[100];
+		int recorridoTam = 0;
 
-		if (find(x, p, recorrido)) {
+		if (find(x, p, recorrido, recorridoTam)) {// FIND MODIFICADO CON EL TAMAÑO DEL RECORRIDO
 			return 0;
 		}
-
+		
 		*p = new NODE(x);
+		recorrido[recorridoTam] = p; //INSERTANDO EL ULTIMO NODO EN EL RECORRIDO
+
+		//AJUSTANDO PESOS DE LOS NODOS
+
+		if (recorridoTam > altura) {
+			for (int i = recorridoTam - 1; i >= 0; i--) {
+				if (*(recorrido[i + 1]) == (*(recorrido[i]))->nodes[1]) {
+					(*(recorrido[i]))->peso += 1;
+					if ((*(recorrido[i]))->peso <= -2 && (*(recorrido[i]))->peso >= 2) {
+
+					}
+				}
+				else {
+					(*(recorrido[i]))->peso -= 1;
+				}
+			}
+		}
+		//if(recorridoTam > 0){
+		//	if ((*(recorrido[recorridoTam - 1]))->nodes[0] == 0 && (*(recorrido[recorridoTam - 1]))->nodes[1] == 0) {
+		//		for (int i = recorridoTam - 1; i >= 0; i--) {
+		//			if (*(recorrido[i + 1]) == (*(recorrido[i]))->nodes[1]) {
+		//				(*(recorrido[i]))->peso += 1;
+		//			}
+		//			else {
+		//				(*(recorrido[i]))->peso -= 1;
+		//			}
+		//		}
+		//	}
+		//}
+
+		//VERIFICANDO SI HAY ALGUN PESO FUERA DEL RANGO DE PESO
+
+		//for (int i = 0; i < recorridoTam; i++) {
+		//	if ((*(recorrido[i]))->peso <= -2 && (*(recorrido[i]))->peso >= 2) {
+		//		//TIPO DE DESBALANCE
+
+
+		//	}
+		//}
+
 		return 1;
 	}
 
@@ -93,7 +140,7 @@ public:
 	void inorder(NODE* k) {
 		if (!k) return;
 		inorder(k->nodes[0]);
-		cout << k->valor <<"->";
+		cout << k->valor <<"<"<<k->peso<<">"<<"->";
 		inorder(k->nodes[1]);
 	}
 
@@ -119,13 +166,15 @@ int main()
 {
 	CTREE arbol;
 	int x;
-	arbol.insert(9);
-	arbol.insert(6);
-	arbol.insert(8);
-	arbol.insert(11);
-	arbol.insert(1);
+	arbol.insert(2);
 	arbol.insert(5);
-	arbol.insert(13);
+	arbol.insert(4);
+	arbol.insert(3);
+	arbol.insert(1);
+	arbol.insert(9);
+	arbol.insert(8);
+	arbol.insert(6);
+	arbol.insert(7);
 	while (1) {
 		cout << "Inorder: ";
 		arbol.inorder(arbol.m_root);
